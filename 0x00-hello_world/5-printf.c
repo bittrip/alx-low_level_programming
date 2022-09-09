82cd60309ef91f3019b4b26120457f849f4ffb48
Akinola Abiodun E <eakinola298@gmail.com>
2022-09-08 20:31:04 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program's entry point.
+ * Description: Prints a text to screen using printf() function.
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+char str[] = "with proper grammar, but the outcome is a piece of art,\n";
+printf("%s", str);
+return (0);
+}
