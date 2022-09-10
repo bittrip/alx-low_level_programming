4da638ae06ba4a67c12e2ae2f92b595f988d2337
bittrip <86340886+bittrip@users.noreply.github.com>
2022-09-10 04:56:51 +0000

    Check 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabets
+ * Description: print the letters of the alphabet in lowercase except e, q
+ * Return: 0, if successful
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+											}
+	putchar(10);
+
+	return (0);
+}
