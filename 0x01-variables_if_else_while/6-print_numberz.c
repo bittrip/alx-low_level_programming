250aeb9761c1bbc31bc93d10891a999c15476eca
bittrip <86340886+bittrip@users.noreply.github.com>
2022-09-10 04:58:51 +0000

    Check 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers
+ * Desciption: print single digit numbers
+ * Return: 0, if successful
+ */
+
+int main(void)
+{
+	int i;
+
+
+	i = 48;
+
+	while (i < 58)
+
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+
+}
