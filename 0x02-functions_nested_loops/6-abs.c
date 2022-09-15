d6301b3abff3278de762acedf0936880413fcc6e
bittrip <86340886+bittrip@users.noreply.github.com>
2022-09-15 17:07:54 +0000

    Check 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ * @n: an integer.
+ * Description: computes the absolute value of an integer.
+ * Return: absolute value of integer
+ */
+
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
