da4b50d24d134922198f87604acdcd9b47242e9c
bittrip <86340886+bittrip@users.noreply.github.com>
2022-09-15 16:47:57 +0000

    Check 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isapha - checks if a character is an alphabet
+ * Description: checks if a character is an alphabet.
+ * @c: c is an ASCII character.
+ * Return: 1, if c is an alphabet, 0 if c is not an alphabet
+ */
+
+int _islower(int c)
+{
+if (c <= 'z' && c >= 'a' && c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+return (0);
+}
