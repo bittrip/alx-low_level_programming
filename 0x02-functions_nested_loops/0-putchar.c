6f9f059c4972ec9cd06941b292daaf52627523e5
bittrip <86340886+bittrip@users.noreply.github.com>
2022-09-15 15:52:38 +0000

    Check 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ * Description: prints _putchar to screen.
+ * Return: 0, if successful.
+ */
+
+int main(void)
+{
+    char str[9] = "_putchar";
+    int i = 0;
+
+    for (i = 0; i < 8; i++)
+    {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
+
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# 0x02-functions_nested_loops
+
+This directory contains solutions to the ALX section of ALX LOW LEVEL PROGRAMMING.
