2e2790c3c3897a993d262aceab5df972ca3d9444
bittrip <86340886+bittrip@users.noreply.github.com>
2022-09-16 06:40:24 +0000

    Check 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and
+ * returns the result.
+ * Description: adds two integers and
+ * returns the result.
+ * Return: integer sum
+ */
+
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return sum;
+}
