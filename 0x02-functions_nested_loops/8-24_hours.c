a7815619d16966934a36aeeb74c34314b4fe1020
bittrip <86340886+bittrip@users.noreply.github.com>
2022-09-16 06:11:44 +0000

    Check 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
-#define MINUTE_MAX 60
-#define HOUR_MAX 24
+#define MINUTE_MAX '60'
+#define HOUR_MAX '24'
 
 /**
  * jack_bauer - prints every minute of Jack Bauer's day starting from 00:00 to 23:59
@@ -11,33 +11,40 @@ void jack_bauer(void)
 {
 int minute;
 int hour;
-for (hour = 0; hour < HOUR_MAX; hour++)
+for (hour = '0'; hour < HOUR_MAX; hour++)
 {
-for (minute=0; minute < MINUTE_MAX; minute++)
+for (minute = '0'; minute < MINUTE_MAX; minute++)
 {
-if (minute < 10 && hour < 10)
+if (minute < '10' && hour < '10')
 {
 _putchar('0');
-_putchar(minute+48);
+_putchar(hour);
 _putchar(':');
 _putchar('0');
-_putchar(hour+48);
+_putchar(minute);
 _putchar('\n');
 }
-else if (minute > 10 && hour < 10)
+else if (minute > '10' && hour < '10')
 {
-_putchar(minute+48);
+_putchar('0');
+_putchar(hour);
+_putchar(':');
+_putchar(minute);
+_putchar('\n');
+}
+else if (minute < '10' && hour > '10')
+{
+_putchar(hour);
 _putchar(':');
 _putchar('0');
-_putchar(hour+48);
+_putchar(minute);
 _putchar('\n');
 }
 else
 {
-_putchar('0');
-_putchar(minute+48);
+_putchar(hour);
 _putchar(':');
-_putchar(hour+48);
+_putchar(minute);
 _putchar('\n');
 }
 }
