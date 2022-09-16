8a17aa8dbfcd6dd2965b6cf34b8eaae80df0829c
bittrip <86340886+bittrip@users.noreply.github.com>
2022-09-16 05:54:37 +0000

    Check 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#define MINUTE_MAX 60
+#define HOUR_MAX 24
+
+/**
+ * jack_bauer - prints every minute of Jack Bauer's day starting from 00:00 to 23:59
+ * Description: prints every minute of Jack Bauer's day starting from 00:00 to 23:59 to screen.
+ */
+
+void jack_bauer(void)
+{
+int minute
+int hour
+for (hour = 0; hour < HOUR_MAX, hour++)
+{
+for (minute=0; minute < MINUTE_MAX, minute++)
+{
+if (minute < 10 && hour < 10)
+{
+_putchar('0');
+_putchar(minute);
+_putchar(':');
+_putchar('0');
+_putchar(hour);
+_putchar('\n');
+}
+else if (minute > 10 && hour < 10)
+{
+_putchar(minute);
+_putchar(':');
+_putchar('0');
+_putchar(hour);
+_putchar('\n');
+}
+else
+{
+_putchar('0');
+_putchar(minute);
+_putchar(':');
+_putchar(hour);
+_putchar('\n');
+}
+}
+}
+}
