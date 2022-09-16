4b98e7f6c61b41c9cdcc0c04b34a22731221b69e
bittrip <86340886+bittrip@users.noreply.github.com>
2022-09-16 06:51:50 +0000

    Check 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers
+ * from n to 98.
+ * Description: prints all natural numbers
+ * from n to 98 to screen.
+ * @n: an integer
+ */
+
+void print_to_98(int n)
+{
+if (n < 98)
+{
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
+}
+else if (n > 98)
+{
+while (n > 98)
+{
+printf("%d, " n);
+n--;
+}
+}
+printf("98\n");
+}
